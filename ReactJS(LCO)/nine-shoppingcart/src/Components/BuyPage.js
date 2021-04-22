441ddcbfb29832ee0936caf4034b4f402793818f
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-04-22 22:06:27 +0530

    Created BuyPage.js component where items are shown.

--- ReactJS(LCO)/nine-shoppingcart/src/Components/BuyPage.js
+++ ReactJS(LCO)/nine-shoppingcart/src/Components/BuyPage.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Axios } from "axios";
+import Axios from "axios";
 import { random, commerce } from "faker";
 import { Container, Col, Row } from "reactstrap";
+
+import CartItem from "./Cartitem"
 
 const apiKey = "";
 
@@ -22,15 +24,43 @@ const Buypage = ({ addInCart }) => {
 
     const fetchPhotos = async () => {
         const { data } = await Axios.get(localUrl)
+
+
+        const { photos } = data;
+
+        const allProducts = photos.map(photo => ({
+            smallImage: photo.src.medium,
+            tinyImage: photo.src.tiny,
+            productName: random.word(),
+            productPrice: commerce.price(),
+            id: random.uuid()
+        }));
+
+        setProduct(allProducts);
+
     };
 
-    const { photos } = data;
 
     useEffect(() => {
         fetchPhotos();
     }, []);
 
+    return (
+        <Container fluid>
+            <h1 className="text-white text-center">
+                Buy Page
+            </h1>
+            <Row>
+                {product.map(product => (
+                    <Col md={4} key={product.id}>
+                        <CartItem product={product} addInCart={addInCart} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    )
+
 
 };
 
-export default BuyPage;
+export default Buypage;
